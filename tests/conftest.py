0d14854cde433cda39aee47990e0053e4354dda8
Charlotte Smith <charlotte.em.smith@gmail.com>
2025-11-10 18:20:18 -0500

    uncommenting conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
-# import pytest
-# from CISC_327_CS import database
+import pytest
+import database
 
-# @pytest.fixture(scope="session", autouse=True)
-# def setup_databse():
-#     print("initializing database.")
-#     database.init_database()
-#     database.add_sample_data()
+@pytest.fixture(scope="session", autouse=True)
+def setup_databse():
+    print("initializing database.")
+    database.init_database()
+    database.add_sample_data()
